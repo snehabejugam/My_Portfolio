42013ba51029b69dd763c9f0482d6f246f053326
Sneha Bejugam <snehabejugam08@gmail.com>
2025-12-22 00:39:55 -0500

    Changed Vite Config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/My_Portfolio/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
